7045dcc8cb9af220ba84f74bc324fbf80c4eea76
Muhammad Fadli Alim Arsani <mfarsani@ucsd.edu>
2021-11-29 08:32:10 -0800

    [CHANGE] improved idents and spacings in MineSweeper.ino

--- Project/V3minesweeper/controller/Arduino/MineSweeper/MineSweeper.ino
+++ Project/V3minesweeper/controller/Arduino/MineSweeper/MineSweeper.ino
@@ -1,7 +1,7 @@
 // Acceleration values recorded from the readAccelSensor() function
 int ax = 0; int ay = 0; int az = 0;
-int ppg = 0;        // PPG from readPhotoSensor() (in Photodetector tab)
-int sampleTime = 0; // Time of last sample (in Sampling tab)
+int ppg = 0;              // PPG from readPhotoSensor() (in Photodetector tab)
+int sampleTime = 0;       // Time of last sample (in Sampling tab)
 const int buttonPin1 = 0; /*12;*/
 const int buttonPin2 = 0; /*12;*/
 int x = 0;
@@ -14,6 +14,7 @@ int currentState2 = 0;
 int lastButtonState2;
 bool vibrate = false;
 unsigned long timer1 = millis();
+
 /*
  * Initialize the various components of the wearable
  */
@@ -45,55 +46,60 @@ void loop() {
     sending = false;
     writeDisplay("Controller: Off", 0, true);
   }
+
   else if(command == "start") {
     sending = true;
     writeDisplay("Controller: On", 0, true);
   }
-  else if(command == "choose"){
+
+  else if(command == "choose") {
     int both = false;
       
-    while(1){
-      //print to display
+    while(1) {
+      // print to display
       writeDisplay("Select Tile!", 0, true);
       String h = String(x)+"x"+String(y);
       writeDisplay(h.c_str(), 1, false);
       
-    // if button 1 ++
+      // if button 1 ++
       currentState1 = !digitalRead(buttonPin1);
       if (currentState1 != lastButtonState1) { 
         if (currentState1) {
           x++;
           both = true;
         }
-        if(x == 7){
+        if(x == 7) {
           x =0;
         }
       }
       lastButtonState1 = currentState1;
 
-    // if button 2 ++
+      // if button 2 ++
       currentState2 = !digitalRead(buttonPin2);
       if (currentState2 != lastButtonState2) { 
         if (currentState2) {
           y++;
           
-        }else{
+        }
+        else {
           both = false;
         }
-        if(y == 7){
+        if(y == 7) {
           y =0;
         }
       }
       lastButtonState2 = currentState2;
 
-    // if button 1 and 2 send and break
-      if(both){
+      // if button 1 and 2 send and break
+      if(both) {
         sendMessage(String(x)+","+ String(y));
         writeDisplay("Selecting",0,true);
         break;
       }
     }
-  }else if(command !=""){
+  }
+
+  else if(command !="") {
     writeDisplay("",0,true);
     writeDisplayCSV(command , 1);
   }
